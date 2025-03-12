56c87fd52113d43ea4f6daec06217b237e92c924
Martin Sanchez <56552217+callsingmarte@users.noreply.github.com>
2025-03-12 16:39:46 +0100

    Primer commit del proyecto SimpleMVCApp

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SimpleMVCApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
